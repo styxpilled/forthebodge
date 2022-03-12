44fce380f02538ab5f6512181063b5bf47539fe2
styxpilled <styxpilled.dev@gmail.com>
2022-03-12 06:30:27 +0100

    feat: working picker demo

--- src/components/Picker.svelte
+++ src/components/Picker.svelte
@@ -1,35 +1,45 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  const m = { x: 0, y: 0 };
+  import { hslToRgb } from '$lib/helpers';
+  const m = { x: 0, y: 0, z: 0 };
   let w, h, picker;
   let rect;
 
   onMount(() => {
     rect = picker.getBoundingClientRect()
   });
-  function handleMousemove(event) {
+  function handleSLMove(event) {
     m.x = event.clientX - rect.left;
     m.y = event.clientY - rect.top;
-    console.log(`${Math.round(m.x/w*255)}, ${Math.round(m.y/h*255)}`);
+    // console.log(`${m.x/w}, ${m.y/h}`);
+  }
+
+  function handleHMove(event) {
+    m.z = event.clientY - rect.top;
+    // console.log(`${Math.round(m.z/h)}`);
+    
   }
 
   function handleMouseclick(event) {
-    console.log(`${Math.round(m.x/w*255)}, ${Math.round(m.y/h*255)}`);
+    console.log(`${Math.round(m.x/w)}, ${Math.round(m.y/h)}`);
   }
 </script>
 
 <div class="picker">
   
-  <div id="color-picker" bind:this={picker} bind:offsetWidth={w} bind:offsetHeight={h} on:mousemove={handleMousemove} on:mousedown={handleMouseclick} class="container">
+  <div id="color-picker" bind:this={picker} bind:offsetWidth={w} bind:offsetHeight={h} on:mousemove={handleSLMove} on:mousedown={handleMouseclick} class="container">
     <div class="bg">
       <div class="bg bg1" />
       <div class="bg bg2" />
     </div>
   </div>
-  <div class="hue">
-    
-  </div>
+  <div class="hue" on:mousemove={handleHMove} />
 </div> 
+<div class="preview">
+  <div class="color" style:background-color="rgb({hslToRgb((m.z/h), (m.x/w), (m.y/h))}">
+    sdadad
+    </div>
+</div>
 <style>
   .picker {
     display: flex;
@@ -65,5 +75,8 @@
     height: 100%;
     background: linear-gradient(to bottom, #ff0000 0%, #ffff00 17%, #00ff00 33%, #00ffff 50%, #0000ff 67%, #ff00ff 83%, #ff0000 100%);
     z-index: 3;
+  }
+  .preview {
+    background-color: hsl(250deg 100 8);
   }
 </style>
